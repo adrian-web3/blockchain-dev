c722f80055f350648225e55ed1e448f9978969bb
adrian-web3 <104831638+adrian-web3@users.noreply.github.com>
2022-05-29 05:19:39 +0200

    Update hardhat.config.js

--- Projects/1-Build a Web3 App with Solidity + Ethereum Smart Contracts/my-wave-portal/hardhat.config.js
+++ Projects/1-Build a Web3 App with Solidity + Ethereum Smart Contracts/my-wave-portal/hardhat.config.js
@@ -1,8 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
 // Import and configure dotenv
 require("dotenv").config();
- 
-module.exports = {
+
+module.exports = { 
   solidity: "0.8.0",
   networks: {
     rinkeby: {
