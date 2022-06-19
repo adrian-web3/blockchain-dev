b7baf1d422d0d30123035be542a36e918398655b
adrian-web3 <104831638+adrian-web3@users.noreply.github.com>
2022-06-19 11:52:22 +0200

    Update WavePortal.sol

--- Projects/1-Build a Web3 App with Solidity + Ethereum Smart Contracts/my-wave-portal/contracts/WavePortal.sol
+++ Projects/1-Build a Web3 App with Solidity + Ethereum Smart Contracts/my-wave-portal/contracts/WavePortal.sol
@@ -9,13 +9,13 @@ contract WavePortal {
 
     event NewWave(address indexed from, uint256 timestamp, string message);
 
-    struct Wave {
+    struct Wave { 
         address waver;
         string message;
         uint256 timestamp;
     }
 
-    Wave[] waves; 
+    Wave[] waves;
 
     constructor() payable {
         console.log("We have been constructed!");
