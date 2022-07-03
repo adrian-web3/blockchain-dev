3106647063c9345601cab217cf5608cfdbddeba5
adrian-web3 <104831638+adrian-web3@users.noreply.github.com>
2022-07-03 11:35:02 +0200

    Update deploy.js

--- Projects/1-Build a Web3 App with Solidity + Ethereum Smart Contracts/my-wave-portal/scripts/deploy.js
+++ Projects/1-Build a Web3 App with Solidity + Ethereum Smart Contracts/my-wave-portal/scripts/deploy.js
@@ -12,11 +12,11 @@ const main = async () => {
 const runMain = async () => {
   try {
     await main();
-    process.exit(0);  
+    process.exit(0);
   } catch (error) {
     console.error(error);
     process.exit(1);
   }
 };
 
-runMain();
+runMain(); 
